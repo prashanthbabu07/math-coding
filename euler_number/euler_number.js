3284ab19d47900774dc9b7a09b1ac8f8034ab3ca
Prashanth Babu T <prashanthbabu07@gmail.com>
2021-05-25 13:51:23 +0530

    euler number

---
+++ euler_number/euler_number.js
@@ -0,0 +1,12 @@
+const euler_constant = n => {
+	sum = 1;
+	denominator = 1;
+	for (i = 1; i <= n; i++) {
+		denominator = denominator * i;
+		sum = sum + (1 / denominator)
+	}
+	return sum;
+}
+
+E = euler_constant(10)
+document.querySelector("#e").innerHTML = E;
